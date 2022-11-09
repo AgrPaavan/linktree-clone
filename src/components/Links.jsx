65780e2e770b8be08541e904e094df0a85c215f1
Paavan Agrawal <33314332+AgrPaavan@users.noreply.github.com>
2022-11-09 22:53:40 +0530

    Remove gh-pages

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -6,7 +6,7 @@ function Links() {
   return (
     <div className="flex flex-col justify-center items-center mt-8 w-full">
       {content.map((e) => (
-        <Link title={e.title} link={e.link} image={e.image} />
+        <Link title={e.title} link={e.link} image={e.image} key={e.id} />
       ))}
     </div>
   );
